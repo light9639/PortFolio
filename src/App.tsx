fd19d87e79eff172dc6bb989649ba7dc8b3b1b38
Lee dong ho <dong963939@gmail.com>
2022-10-01 20:33:15 +0900

    :heavy_plus_sign: netlify.toml 추가.
    netlify.toml 추가.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom';
 import About from './components/About';
 import DarkModeToggle from './components/DarkModeToggle';
