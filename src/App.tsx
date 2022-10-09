d94797d6ad38c19d28896ca7e826a7a1276d1868
Lee dong ho <dong963939@gmail.com>
2022-10-09 23:40:11 +0900

    :art: 추가 수정.
    추가 수정.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import About from './components/About';
 import DarkModeToggle from './components/DarkModeToggle';
-import Footer from "./components/Footer";
 import Introduce from './components/Introduce';
-import Nav from "./components/Nav";
 import Section from './components/Section';
 import Undefined from './components/Undefined';
 
 function App() {
   return (
     <div className="App">
-
-      <Nav />
 
       <div className='pt-16 pb-32'>
         <Routes>
@@ -23,7 +19,6 @@ function App() {
         </Routes>
       </div>
 
-      <Footer />
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
+    <Nav />
     <App />
+    <Footer />
   </BrowserRouter>
 )
