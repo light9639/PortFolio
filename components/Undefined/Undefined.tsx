5c57da16a719cdbe79966d64e9b26932d3801002
Lee dong ho <dong963939@gmail.com>
2022-12-21 11:30:11 +0900

    ✅ 주석 제거 및 Undefined 수정.

--- components/Undefined/Undefined.tsx
+++ components/Undefined/Undefined.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import Animation from '../Animation/Animation'
-import "./UndefinedStyle.css";
+import { useNavigate } from 'react-router-dom';
 
 export default function Undefined(): JSX.Element {
+    const navigate = useNavigate();
+
     return (
         <React.Fragment>
-            <div className='Undefined' style={{ maxWidth: "100%", width: "600px", margin: "auto" }}>
+            <div className='Undefined max-w-full w-[90%] md:w-1/3 mx-auto'>
 
                 <Animation />
 
                 <div className="mx-auto text-center">
-                    <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
-                    <p className="mt-5 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
-                    <button className="px-8 py-3 font-semibold rounded Blue_box">Back to homepage</button>
+                    <p className="text_gradient2 text-2xl font-semibold md:text-[1.7rem]">Sorry, we couldn't find this page.</p>
+                    <p className="mt-5 mb-8 text-sm md:text-[1.1rem] dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
+                    <button
+                    onClick={() => {navigate("/");}}
+                        className="px-8 py-3 font-semibold rounded Blue_box hover:opacity-75 transition"
+                    >Back to homepage</button>
                 </div>
             </div>
         </React.Fragment>
