eb728a36f5ef03b8fb4c1da80c872bbd15828619
Lee dong ho <dong963939@gmail.com>
2023-01-31 11:28:42 +0900

    :art: css scss로 변경
    css scss로 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import '@styles/style.css'
-import '@styles/DarkMode.css'
-import '@styles/gradient.css'
+import '@styles/style.scss'
+import '@styles/DarkMode.scss'
+import '@styles/gradient.scss'
 import Footer from "@components/Footer/Footer";
 import Nav from "@components/Nav/Nav";
 import { BrowserRouter } from 'react-router-dom'
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <Nav />
-        <App />
+      <App />
       <Footer />
     </BrowserRouter>
   </QueryClientProvider>
