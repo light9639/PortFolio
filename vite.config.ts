73efa42a72514e53f79b73c721c8854a6a00ee40
Lee dong ho <dong963939@gmail.com>
2023-03-30 15:38:16 +0900

    :art: 추가 수정.
    추가 수정.

--- vite.config.ts
+++ vite.config.ts
@@ -6,19 +6,5 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   base: process.env.NODE_ENV === 'development' ? '/' : './',
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.').at(1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img';
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-      },
-    },
-  },
+  
 })
